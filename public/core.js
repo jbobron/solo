e5fcf0a613d9a29dfd40aaf940ffe0f464f0edec
Jake Obron <jbobron@gmail.com>
2015-01-19 15:39:58 -0800

    refactor

---
+++ public/core.js
@@ -0,0 +1,48 @@
+var supperclub = angular.module('supperclub', []);
+
+function mainController($scope, $http) {
+    $scope.formData = {};
+
+    // when landing on the page, get all meals and show them
+    $http.get('/api/meals')
+        .success(function(data) {
+            $scope.meals = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createMeal = function() {
+        $http.post('/api/meals', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {}; // clear the form so our user is ready to enter another
+                $scope.meals = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteMeal = function(id) {
+        $http.delete('/api/meals/' + id)
+            .success(function(data) {
+                $scope.meals = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
+
+
+
+
+
+
+
--- server/server.js
+++
@@ -1,9 +0,0 @@
-var app = require('./server-config.js');
-
-// var port = process.env.PORT || 4568;
-
-var port = 8000;
-
-app.listen(port);
-
-console.log('Server now listening on port ' + port);
